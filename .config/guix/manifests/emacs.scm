73035c65f96727eee80b8c10976c6ac4bcaeebb7
Grimpper <sergio.pastorperez@gmail.com>
2022-09-19 19:14:33 +0200

    Fixed emacs-guix problem with the new geiser

--- .config/guix/manifests/emacs.scm
+++ .config/guix/manifests/emacs.scm
@@ -1,11 +1,36 @@
-(specifications->manifest
- '(
-   "emacs"
-   "emacs-vterm"
-   "emacs-guix"
-   "emacs-geiser"
-   "emacs-geiser-guile"
+(use-modules (guix transformations))
 
-   ;; Enable man pages for the profile
-   "man-db"
-   ))
+(define transform1
+  (options->transformation
+    '((with-commit
+        .
+        "emacs-geiser-guile=0.23.2")
+      (with-commit
+        .
+        "emacs-geiser=0.23.2"))))
+
+(packages->manifest
+  (list (transform1
+          (specification->package "emacs-geiser"))
+        (transform1
+          (specification->package "emacs-geiser-guile"))
+        (transform1 (specification->package "emacs"))
+        (transform1
+          (specification->package "emacs-guix"))
+        (transform1
+          (specification->package "emacs-vterm"))))
+
+;; (use-modules (guix transformations))
+
+;; ((options->transformation '((with-commit . "emacs-geiser=bc0dde310492de733f9ec528d7aeddd57093d05e")
+;;                             (with-commit . "emacs-geiser-guile=c641fcc50b6b86ca95743122b5206cdcd475f96e")))
+;;  (specifications->manifest
+;;   '(
+;;     "emacs"
+;;     "emacs-vterm"
+;;     "emacs-guix"
+;;     "emacs-geiser"
+;;     "emacs-geiser-guile"
+
+;;     ;; Enable man pages for the profile
+;;     "man-db")))

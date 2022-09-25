b4cec4c415f2d5b2289b207f7ef7cb82199f585f
Grimpper <sergio.pastorperez@gmail.com>
2022-09-25 14:21:40 +0200

    Pinned geiser/guile packages to a commit (shadowed system package)

--- .config/guix/manifests/emacs.scm
+++ .config/guix/manifests/emacs.scm
@@ -14,23 +14,9 @@
           (specification->package "emacs-geiser"))
         (transform1
           (specification->package "emacs-geiser-guile"))
-        (transform1 (specification->package "emacs"))
+        (transform1
+          (specification->package "emacs"))
         (transform1
           (specification->package "emacs-guix"))
         (transform1
           (specification->package "emacs-vterm"))))
-
-;; (use-modules (guix transformations))
-
-;; ((options->transformation '((with-commit . "emacs-geiser=bc0dde310492de733f9ec528d7aeddd57093d05e")
-;;                             (with-commit . "emacs-geiser-guile=c641fcc50b6b86ca95743122b5206cdcd475f96e")))
-;;  (specifications->manifest
-;;   '(
-;;     "emacs"
-;;     "emacs-vterm"
-;;     "emacs-guix"
-;;     "emacs-geiser"
-;;     "emacs-geiser-guile"
-
-;;     ;; Enable man pages for the profile
-;;     "man-db")))

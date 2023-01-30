4d06ca01b4a32f19f353f74f0a6cab288ca496b4
Sergio Pastor Pérez <sergio.pastorperez@gmail.com>
2023-01-30 19:47:46 +0100

    Removed commit lock from emacs-guix (problems seem fixed)

--- .config/guix/manifests/emacs.scm
+++ .config/guix/manifests/emacs.scm
@@ -1,19 +1,14 @@
-(use-modules (guix transformations))
+(specifications->manifest
+ '(
+   ;; Emacs packages
+   "emacs"
+   "emacs-guix"
+   "emacs-geiser"
+   "emacs-geiser-guile"
+   "emacs-vterm"
 
-(define transform1
-  (options->transformation
-    '((with-commit . "emacs-geiser-guile=0.23.2")
-      (with-commit . "emacs-geiser=0.23.2"))))
-
-(packages->manifest
-  (list (transform1
-          (specification->package "emacs-geiser"))
-        (transform1
-          (specification->package "emacs-geiser-guile"))
-        (transform1
-          (specification->package "emacs"))
-        (transform1
-          (specification->package "emacs-guix"))
-        (transform1
-         (specification->package "emacs-vterm"))
-        (specification->package "fish")))
+   ;; External dependencies
+   "fish"
+   "ripgrep"
+   "git"
+   ))
